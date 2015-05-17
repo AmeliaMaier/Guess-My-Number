b9f58d4a80b8c1e705bbce10763c1dd4fd2619cb
AmeliaMaier <Amelia.Maier.Jobsearch@gmail.com>
2015-05-17 13:11:08 -0500

    Rename

--- Guess My Number/src/GuessMyName/GuessMyNumber.java
+++ Guess My Number/src/GuessMyName/GuessMyNumber.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package guess.my.number;
+package GuessMyName;
 
 import java.util.Random;
 import java.util.Scanner;
